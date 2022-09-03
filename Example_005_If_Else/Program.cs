16a104de1c146ecb22f650671d26174a7bac98fb
«Edd» <eddsavil@gmail.com>
2022-09-03 13:56:48 +0300

    add Ex005 If Else

---
+++ Example_005_If_Else/Program.cs
@@ -0,0 +1,12 @@
+﻿Console.Write("input name: ");
+string username = Console.ReadLine();
+
+if(username.ToLower() == "маша")
+{
+    Console.WriteLine("Ура, это же МАША");
+}
+else
+{
+    Console.WriteLine("Hello, ");
+    Console.WriteLine(username);
+}
